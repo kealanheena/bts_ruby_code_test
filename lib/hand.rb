7509254bdc57020ccd22d9869a332b9b17af5c08
kealanheena <kealanheena@gmail.com>
2021-08-01 11:21:56 +0100

    Refactor: make changes to have rubocop passing and add comments to explain certain lines of code

--- lib/hand.rb
+++ lib/hand.rb
@@ -9,13 +9,14 @@ class Hand
 
   def initialize(cards, deck = Deck.new().cards)
     @cards = cards.upcase
+    @cards_faces = get_cards_faces
+    @cards_suits = get_cards_suits
     @deck = deck
   end
 
   def identify
     if valid_hand?
-      cards_faces = get_cards_faces
-      return identify_pair(cards_faces) if duplicate_card_faces?(cards_faces)
+      return identify_pair if duplicate_card_faces?
 
       return 'straight flush' if straight_flush?
 
@@ -41,70 +42,23 @@ class Hand
     @cards.split.map { |card| card[0...-1] }
   end
 
+  # this returns a hash with the card face (key) and 
+  # the amount of that face in this hand (value)
+  def get_faces_count
+    @cards_faces.each_with_object(Hash.new(0)) { |key, hash| 
+      hash[key] += 1 
+    }
+  end
+
   def get_numeric_cards_faces
     numeric_values = { 'J' => 11, 'Q' => 12, 'K' => 13, 'A' => 14 }
     # this returns an array with only the cards faces and replaces 
     # any instance of J Q K or A with thier numeric value or converts
     # the string to an integer
-    get_cards_faces.map { |card| numeric_values[card] || card.to_i }
+    @cards_faces.map { |card| numeric_values[card] || card.to_i }
   end
 
-  def duplicate_card_faces?(cards_faces)
-    cards_faces.uniq().length != REQUIRED_AMOUNT_OF_CARDS
-  end
-
-  def flush?
-    get_cards_suits.uniq.length == 1
-  end
-
-  def straight?
-    # sort the array and set the first number in array
-    sorted_numeric_cards = get_numeric_cards_faces.sort
-    previous_card = sorted_numeric_cards[0]
-    first_card = sorted_numeric_cards[0] 
-    last_card = sorted_numeric_cards[-1]
-
-    return loop_around_straight?(sorted_numeric_cards) if loop_around?(first_card, last_card)
-
-    # go through each card in the array and check that the it is 
-    # equal to the previous card plus 1 (if it isn't return false)
-    sorted_numeric_cards[1, REQUIRED_AMOUNT_OF_CARDS].each do |card|
-      return false if previous_card + 1 != card
-
-      previous_card = card
-    end
-
-    true
-  end
-
-  def straight_flush?
-    straight? && flush?
-  end
-
-  def loop_around_straight?(sorted_numeric_cards)
-    starting_index = 1
-    previous_card = sorted_numeric_cards[0]
-
-    sorted_numeric_cards[starting_index, REQUIRED_AMOUNT_OF_CARDS].each_with_index do |card, index|
-      if previous_card + 1 != card 
-        starting_index = index + 1
-        previous_card = card
-        break
-      end
-
-      previous_card = card
-    end
-
-    sorted_numeric_cards[starting_index + 1, REQUIRED_AMOUNT_OF_CARDS].each do |card|
-      return false if previous_card + 1 != card 
-
-      previous_card = card
-    end
-
-    return true
-  end
-
-  def update_array_of_pairs(faces_count, pairs)
+  def update_pairs_array(faces_count, pairs)
     faces_count.each do |_key, value|
       case value
         when 4
@@ -117,29 +71,17 @@ class Hand
     end
   end
 
-  def identify_pair(cards_faces)
+  def identify_pair
     pairs = []
-    faces_count = get_faces_count(cards_faces)
+    faces_count = get_faces_count
 
-    update_array_of_pairs(faces_count, pairs)
+    update_pairs_array(faces_count, pairs)
     
     return pairs[0] if pairs.length == 1
     
     return 'two pair' if pairs.uniq().length == 1
 
     return 'full house'
-  end
-
-  # this returns a hash with the card face (key) and 
-  # the amount of that face in this hand (value)
-  def get_faces_count(cards_faces)
-    cards_faces.each_with_object(Hash.new(0)) { |key, hash| 
-      hash[key] += 1 
-    }
-  end
-
-  def loop_around?(first_card, last_card)
-    first_card == 2 && last_card == 14
   end
 
   # this honestly had me torn because I think it was previously more readable
@@ -152,6 +94,66 @@ class Hand
     # 2. checking if the amount of remaining cards the same as 
     #    AMOUNT_OF_CARDS_IN_A_DECK minus REQUIRED_AMOUNT_OF_CARDS
     (@deck - @cards.split()).length == AMOUNT_OF_CARDS_IN_A_DECK - REQUIRED_AMOUNT_OF_CARDS
+  end
+
+  def duplicate_card_faces?
+    @cards_faces.uniq().length != REQUIRED_AMOUNT_OF_CARDS
+  end
+
+  def flush?
+    @cards_suits.uniq.length == 1
+  end
+
+  def straight?
+    # sort the array and set the first number in array
+    numeric_cards = get_numeric_cards_faces.sort
+    prev_card = numeric_cards[0]
+    first_card = numeric_cards[0] 
+    last_card = numeric_cards[-1]
+
+    # checks if loop around straight is possible and then checks for it
+    # (to be possible the first card has to be 2 and the last card has to be A (14))
+    return loop_around_straight?(numeric_cards, prev_card) if loop_around?(first_card, last_card)
+
+    # go through each card in the array and check that the it is 
+    # equal to the previous card plus 1 (if it isn't return false)
+    cards_increment_by_one?(numeric_cards[1, REQUIRED_AMOUNT_OF_CARDS], prev_card)
+  end
+
+  def straight_flush?
+    straight? && flush?
+  end
+
+  def loop_around?(first_card, last_card)
+    first_card == 2 && last_card == 14
+  end
+
+  def loop_around_straight?(numeric_cards, prev_card)
+    starting_index = 1
+
+    # checks if the first X numers increment by 1
+    numeric_cards[starting_index, REQUIRED_AMOUNT_OF_CARDS].each_with_index do |card, index|
+      if prev_card + 1 != card 
+        starting_index = index + 1
+        prev_card = card
+        break
+      end
+
+      prev_card = card
+    end
+
+    # checks if the remaining numbers in 
+    cards_increment_by_one?(numeric_cards[starting_index + 1, REQUIRED_AMOUNT_OF_CARDS], prev_card)
+  end
+
+  def cards_increment_by_one?(numeric_cards, prev_card)
+    numeric_cards.each do |card|
+      return false if prev_card + 1 != card 
+
+      prev_card = card
+    end
+
+    true
   end
 
 end
